406c2667375b6c359b55731aa944c3aabcc3b686
hadarsharon <hadar.sharon94@gmail.com>
2024-02-24 23:28:13 +0200

    generic dataframe type

--- compars/dataframe/types.py
+++ compars/dataframe/types.py
@@ -1,1 +1,11 @@
-"""Generic (bear-agnostic) DataFrame type"""
+from pandas.core.frame import DataFrame as PandasDataFrame
+from polars.dataframe.frame import DataFrame as PolarsDataFrame
+from pyspark.sql.dataframe import DataFrame as SparkDataFrame
+from dask.dataframe.core import DataFrame as DaskDataFrame
+from modin.pandas.dataframe import DataFrame as ModinDataFrame
+
+
+class DataFrame:
+    """Generic (bear-agnostic) DataFrame type"""
+
+    def __init__(self, df: PolarsDataFrame | PandasDataFrame | SparkDataFrame | DaskDataFrame | ModinDataFrame): ...
