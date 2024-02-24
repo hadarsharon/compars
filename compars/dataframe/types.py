02b1fbe381416bf4e1a41456f2b3d40a3f744221
hadarsharon <hadar.sharon94@gmail.com>
2024-02-24 22:54:58 +0200

    WIP

---
+++ compars/dataframe/types.py
@@ -0,0 +1,5 @@
+from polars import DataFrame as PolarsDataFrame
+from pandas import DataFrame as PandasDataFrame
+from pyspark.sql.dataframe import DataFrame as SparkDataFrame
+
+"""Generic (bear-agnostic) DataFrame type"""
